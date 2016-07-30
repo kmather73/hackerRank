4b7810bc1eb09f7e0704ccad84fb28308390635b
Kevin <kmather73@users.noreply.github.com>
2016-07-30 18:57:26 -0500

    Create  CamelCase.cpp

---
+++ Algorithms/Strings/ CamelCase.cpp
@@ -0,0 +1,39 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    string s;
+    cin >> s;
+    
+    int letters = 1;
+    for(char c : s){
+        if( c >= 'A' && c <= 'Z')
+            ++letters;
+    }
+    
+    cout << letters << endl;
+    return 0;
+}
