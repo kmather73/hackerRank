cac1bf77811f5ed5bc38c3381279d08e48c04bbb
Kevin <kmather73@users.noreply.github.com>
2016-07-30 19:48:40 -0500

    Create Divisible Sum Pairs.cpp

---
+++ Algorithms/Implementation/Divisible Sum Pairs.cpp
@@ -0,0 +1,45 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    int k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for(int a_i = 0;a_i < n;a_i++){
+       cin >> a[a_i];
+    }
+    
+    unordered_map<int, int> map;
+    int count = 0;
+    for(int i=0; i < n; ++i){
+        count += map[(k-a[i]%k)%k];
+        ++map[a[i]%k];
+    }
+    
+    cout << count << endl;
+    return 0;
+}
