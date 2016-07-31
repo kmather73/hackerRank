fe7f30f9a741af0c2d61c85bee64a13b4c73ff15
Kevin <kmather73@users.noreply.github.com>
2016-07-30 19:32:21 -0500

    Create Compare the Triplets.cpp

---
+++ Algorithms/Warmup/Compare the Triplets.cpp
@@ -0,0 +1,44 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int a0;
+    int a1;
+    int a2;
+    cin >> a0 >> a1 >> a2;
+    int b0;
+    int b1;
+    int b2;
+    cin >> b0 >> b1 >> b2;
+    
+    int pa=0, pb=0;
+    
+    pa = (a0>b0) + (a1>b1) +(a2>b2);
+    pb = (a0<b0) + (a1<b1) +(a2<b2);
+    
+    cout << pa << " " << pb <<endl;
+    return 0;
+}
