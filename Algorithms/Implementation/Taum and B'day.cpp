030400c9b40f3a1b456ab01322711adfc85f96d6
Kevin <kmather73@users.noreply.github.com>
2016-08-01 22:14:11 -0500

    Create Taum and B'day.cpp

---
+++ Algorithms/Implementation/Taum and B'day.cpp
@@ -0,0 +1,46 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int t;
+    cin >> t;
+    for(int a0 = 0; a0 < t; a0++){
+        long b;
+        long w;
+        cin >> b >> w;
+        long x;
+        long y;
+        long z;
+        cin >> x >> y >> z;
+        
+        x = min(x, y+z);
+        y = min(y, x+z);
+        
+        long cost = x*b + y*w;
+        cout << cost << endl;
+    }
+    return 0;
+}
